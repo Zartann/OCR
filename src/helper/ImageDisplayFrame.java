d980ae6dc810fc59a4aff79fdc4e2b2a7012914c
alexisld <alexisledantec@gmail.com>
2014-10-06 16:47:37 +0200

    Lecture des données d'apprentissage

---
+++ src/helper/ImageDisplayFrame.java
@@ -0,0 +1,29 @@
+package helper;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+
+public class ImageDisplayFrame extends JFrame {
+
+	private ImageDisplayPannel pan;
+	private final static int defaultWidth = 280, defaultHeight = 280;
+	
+	public ImageDisplayFrame(BufferedImage img){        
+		this.setTitle("Animation");
+		this.setSize(defaultWidth, defaultHeight);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		pan = new ImageDisplayPannel(img);
+		this.setContentPane(pan);
+		this.setVisible(true);
+	}
+
+	public void changeImage(BufferedImage img){
+		pan.changeImage( img );
+		repaint();
+	}
+	
+}
---
+++ src/helper/ImageDisplayPannel.java
@@ -0,0 +1,32 @@
+package helper;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JPanel;
+
+
+public class ImageDisplayPannel extends JPanel {
+	
+	private BufferedImage image = new BufferedImage(28, 28, BufferedImage.TYPE_BYTE_GRAY);
+	
+	public ImageDisplayPannel(BufferedImage img){
+		image = img;
+	}
+
+	public void paintComponent(Graphics g){
+		g.setColor(Color.WHITE);
+		g.clearRect(0, 0, getWidth(), getHeight());
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		g.drawImage(image, 0, 0, getWidth(), getHeight(),
+				null);
+	}
+
+	public void changeImage(BufferedImage img){
+		image = img;
+		repaint();
+	}
+	
+}
